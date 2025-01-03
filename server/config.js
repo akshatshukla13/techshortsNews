ac8201fe94ae8fa9b54c9e9b3b2893cdfeb03870
Akshat-Sh <akshatvijay1302@gmail.com>
2025-01-03 16:03:16 +0530

    update client origin to production URL

--- server/config.js
+++ server/config.js
@@ -3,6 +3,6 @@ export const config = {
   newsApiKey: '86b20319703149738210e481818a24fc',
   newsApiBaseUrl: 'https://newsapi.org/v2',
   // clientOrigin: 'https://tech-inshorts.vercel.app'
-  clientOrigin: 'http://localhost:5173'
+  clientOrigin: 'https://techshorts-news-client.vercel.app'
   //clientOrigin: '*'
 };
